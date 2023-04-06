1ba59eb284bbe386d7fef6ad144e96dee74a1ad5
Hemanth-Kagitha <123528403+Hemanth-Kagitha@users.noreply.github.com>
2023-04-05 20:37:09 -0500

    1234

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # s23wb91kagitha
 
  Render Link : https://s23wb91kagitha.onrender.com
+
+ Kite Attributes :  [Kite_color,Kite_length,Kite_cost, Kite_side]
--- app.js
+++ app.js
@@ -7,6 +7,11 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
+var kiteRouter = require('./routes/kite');
+var boardRouter = require('./routes/board');
+var selectorRouter = require('./routes/selector');
+
+
 var app = express();
 
 // view engine setup
@@ -21,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use('/kite', kiteRouter);
+app.use('/board', boardRouter);
+app.use('/selector',selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
